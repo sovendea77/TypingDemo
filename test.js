f6778a17cfe47104bc78e11510f53b1045d2c748
sovendea77 <115879243+sovendea77@users.noreply.github.com>
2023-04-09 01:14:55 +0800

    revise

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 const a = document.querySelector('.typing-demo')
 const text  = a.innerText
-a.style.width = text.length + 'ch'
+a.style.width = text.length + 'em'
+
+function replaceword()
+{
+    a.innerText = "恭喜发财"
+    a.style.width = a.innerText.length + 'em'
+    a.classList.remove('type-demo')
+    a.classList.add('test-transform')
+}
+setTimeout(replaceword, 5000); 
